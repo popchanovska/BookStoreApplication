7c5bdf5817272f59b29ca8349249c34abc2b9eba
popchanovska <e.popchanovska@gmail.com>
2024-08-19 01:25:37 +0200

    Initial code: Add Web, Repo, Service, and Domain layers

---
+++ BookApplication/BookApplication.Repository/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BookApplication.Repository
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BookApplication/BookApplication.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApplication.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
