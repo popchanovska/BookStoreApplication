404573ea6cd8cb36d1997e20266a5ca1566915ba
bojanognenov <ognenovbojan@gmail.com>
2025-01-18 12:09:48 +0100

    ^^

---
+++ BookAdminApplication/BookAdminApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookAdminApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
